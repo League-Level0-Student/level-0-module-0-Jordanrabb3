d5de1bd74ce2d31f89272d5c2317bd79eb216eb5
root <root@3cbde7b472e3>
2021-02-24 02:30:01 +0000

    keep going

--- src/_03_print_and_popups/_4_quiz_game/QuizGame.java
+++ src/_03_print_and_popups/_4_quiz_game/QuizGame.java
@@ -12,16 +12,25 @@ public class QuizGame {
 				// 2.  Ask the user a question 
 				String question =JOptionPane.showInputDialog("how old are you?");
 				// 3.  Use an if statement to check if their answer is correct
-				if (question.equals("17")) {
+				if (question.equals("18")) {
 					JOptionPane.showMessageDialog(null, "You are correct");
 					score = score + 1;
+				} else {
+					JOptionPane.showMessageDialog(null,"that is incorrect");
+							
 				}
 				// 4.  if the user's answer was correct, add one to their score 
 				
 		// MAKE MORE QUESTIONS. Ask more questions by repeating the above 
 				// Option: Subtract a point from their score for a wrong answer
+		question =JOptionPane.showInputDialog("what school do you go to");
 		
-		
+		if (question.contentEquals("Millikan")) {
+			JOptionPane.showMessageDialog(null, "oh really?");
+			score = score +1;
+		}else {
+			JOptionPane.showMessageDialog(null, "eww trashy");
+		}
 		// After all the questions have been asked, tell the user their final score 
 		
 	}
